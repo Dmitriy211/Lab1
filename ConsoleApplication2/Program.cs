8fcb419037c40425e1b0b5e748af39101c2a1a0a
Dmitriy211 <dmitriy.211@mail.ru>
2017-01-16 17:49:41 +0600

    Done

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class student
+    {
+        public string firstname;
+        public string lastname;
+        public int age;
+        public double gpa;
+    }
+        
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            student s = new student();
+
+            s.firstname = Console.ReadLine();
+            s.lastname = Console.ReadLine();
+            s.age = int.Parse(Console.ReadLine());
+            s.gpa = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(s.firstname + ' ' + s.lastname + ' ' + s.age + ' ' + s.gpa);
+        }
+    }
+}
